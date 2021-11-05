5ac065aecfa8de2f61ca91833b17bcb1388c22bf
Lorina Poland <lorinapoland@gmail.com>
2021-11-05 16:08:20 -0700

    cleanup and comment

--- src/bin/main-astra.rs
+++ src/bin/main-astra.rs
@@ -6,33 +6,22 @@ use stargate_grpc::*;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
 
-    // For Stargate OSS and Astra DB, create client:
+    // For Astra DB: create a client
     let mut client = StargateClient::builder()
 
-    // For Stargate OSS running locally in docker container, set connect information:
-    .uri("http://localhost:8090/")?                           // replace with a proper address
-    .auth_token(AuthToken::from_str("721e9c04-e121-4bf4-b9a6-887ebeae2bc5")?)    // replace with a proper token
-
     // For Astra DB, set connect information:
-    // .uri("https://a2b4465c-e7a4-4cb7-a4a4-c829f0ef10d6-us-west1.apps.astra.datastax.com/stargate")?
-    // .auth_token(AuthToken::from_str("AstraCS:uuwizlOZhGxrUxaOqHPLAGCK:b4296e99a9f801d78043272b0efd79dca115b1fd95765780df36ed3ada87ff9b")?)                                         
-    // .tls(Some(client::default_tls_config()?))   // optional
+    .uri("https://0ba933af-90c2-46e9-887a-a387ba75411b-westus2.apps.astra-dev.datastax.com/stargate")?
+    .auth_token(AuthToken::from_str("AstraCS:YUskXkABjYztQJfTHNkOGHOI:09fa16889d7217b23b3854d3b2858a17b895ec272494a1539e47200b87567f26")?)                                         
+    .tls(Some(client::default_tls_config()?))   // optional
     
     .connect()
     .await?;
 
     println!("created client {:?}", client);
+    
+    // For Astra DB: create a keyspace in the Astra dashboard
 
-    // For Stargate OSS only: create a keyspace
-    // For Astra DB: delete and create a keyspace in the Astra dashboard
-    let create_keyspace = Query::builder()
-        .query("CREATE KEYSPACE IF NOT EXISTS test WITH REPLICATION = {'class':'SimpleStrategy', 'replication_factor':1};")
-        .build();
-    client.execute_query(create_keyspace).await?;
-
-    println!("created keyspace");
-
-    // For Stargate OSS and Astra DB: create a table
+    // For Astra DB: create a table
     let create_table = Query::builder()
         // .keyspace("test")
         .query(
@@ -44,11 +33,10 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
      println!("created table");
 
-    // For Stargate OSS and Astra DB: INSERT two rows/records
+    // For Astra DB: INSERT two rows/records
 	//  Two queries will be run in a batch statement
     let batch = Batch::builder()
-        .keyspace("test")                   // set the keyspace the query applies to
-        .consistency(Consistency::One)      // set consistency level
+        //.keyspace("test")                   // set the keyspace the query applies to
         .query("INSERT INTO test.users (firstname, lastname) VALUES ('Lorina', 'Poland');") 
         .query("INSERT INTO test.users (firstname, lastname) VALUES ('Doug', 'Wettlaufer');")               
         .build();
@@ -56,11 +44,10 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     println!("insert data");
 
-    // For Stargate OSS and Astra DB: SELECT the data to read from the table
+    // For Astra DB: SELECT the data to read from the table
     // Select/query some data from the keyspace.table
     let query = Query::builder()
-        .keyspace("test") 
-        .consistency(Consistency::One) 
+        //.keyspace("test") 
         .query("SELECT firstname, lastname FROM test.users;")               
         .build();  
 
--- src/bin/main-sgoss.rs
+++ src/bin/main-sgoss.rs
@@ -6,25 +6,18 @@ use stargate_grpc::*;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
 
-    // For Stargate OSS and Astra DB, create client:
+    // For Stargate OSS: create A client
     let mut client = StargateClient::builder()
 
     // For Stargate OSS running locally in docker container, set connect information:
     .uri("http://localhost:8090/")?                           // replace with a proper address
-    .auth_token(AuthToken::from_str("721e9c04-e121-4bf4-b9a6-887ebeae2bc5")?)    // replace with a proper token
-
-    // For Astra DB, set connect information:
-    // .uri("https://a2b4465c-e7a4-4cb7-a4a4-c829f0ef10d6-us-west1.apps.astra.datastax.com/stargate")?
-    // .auth_token(AuthToken::from_str("AstraCS:uuwizlOZhGxrUxaOqHPLAGCK:b4296e99a9f801d78043272b0efd79dca115b1fd95765780df36ed3ada87ff9b")?)                                         
-    // .tls(Some(client::default_tls_config()?))   // optional
-    
+    .auth_token(AuthToken::from_str("721e9c04-e121-4bf4-b9a6-887ebeae2bc5")?)    // replace with a proper token  
     .connect()
     .await?;
 
     println!("created client {:?}", client);
 
     // For Stargate OSS only: create a keyspace
-    // For Astra DB: delete and create a keyspace in the Astra dashboard
     let create_keyspace = Query::builder()
         .query("CREATE KEYSPACE IF NOT EXISTS test WITH REPLICATION = {'class':'SimpleStrategy', 'replication_factor':1};")
         .build();
@@ -32,7 +25,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     println!("created keyspace");
 
-    // For Stargate OSS and Astra DB: create a table
+    // For Stargate OSS: create a table
     let create_table = Query::builder()
         // .keyspace("test")
         .query(
@@ -44,7 +37,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
      println!("created table");
 
-    // For Stargate OSS and Astra DB: INSERT two rows/records
+    // For Stargate OSS: INSERT two rows/records
 	//  Two queries will be run in a batch statement
     let batch = Batch::builder()
         .keyspace("test")                   // set the keyspace the query applies to
@@ -56,7 +49,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     println!("insert data");
 
-    // For Stargate OSS and Astra DB: SELECT the data to read from the table
+    // For Stargate OSS: SELECT the data to read from the table
     // Select/query some data from the keyspace.table
     let query = Query::builder()
         .keyspace("test") 
